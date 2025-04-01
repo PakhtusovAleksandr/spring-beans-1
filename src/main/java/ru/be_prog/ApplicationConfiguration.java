dfd90032f5155a83998e75fd10242c25edd90c31
PakhtusovAleksandr <pakhtusov.av@yandex.ru>
2025-04-01 21:45:10 +0300

    firstTask
    firstTask

---
+++ src/main/java/ru/be_prog/ApplicationConfiguration.java
@@ -0,0 +1,42 @@
+package ru.be_prog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "ru.be_prog")
+public class ApplicationConfiguration {
+
+    @Bean(name = "hello-bean")
+    public SpringHelloWorld getHelloBean() {
+        SpringHelloWorld bean = new SpringHelloWorld();
+        bean.setMessage("Hello World from Spring!!!");
+        return bean;
+    }
+
+@Bean(name = "car-models")
+@Scope("prototype")
+public Car getCarModels(){
+        Car bean = new Car();
+        bean.setBrand(randomCarBrand());
+                return bean;
+}
+
+
+    private String randomCarBrand() {
+        int randomIndex = new Random().nextInt(CAR_BRANDS.size());
+        return CAR_BRANDS.get(randomIndex);
+    }
+
+    public static final List<String> CAR_BRANDS = new ArrayList<>(
+            Arrays.asList("Audi", "BMW", "Dodge", "Lada", "Lamborghini", "Land Rover", "Volvo", "Volkswagen", "Porsche")
+    );
+
+}
---
+++ src/main/java/ru/be_prog/SpringBeansApplication.java
@@ -0,0 +1,16 @@
+package ru.be_prog;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringBeansApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+        SpringHelloWorld springHelloWorld = (SpringHelloWorld) context.getBean("hello-bean");
+        System.out.println(springHelloWorld.getMessage());
+
+        Car car = (Car) context.getBean("car-models");
+        System.out.println(car.getBrand());
+    }
+}
