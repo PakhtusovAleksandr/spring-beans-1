43701916cc77ee2d2cf68a33eb26694d90b64aa9
PakhtusovAleksandr <pakhtusov.av@yandex.ru>
2025-04-01 21:42:33 +0300

    firstTask

---
+++ src/main/java/ru/be_prog/Car.java
@@ -0,0 +1,9 @@
+package ru.be_prog;
+
+public class Car {
+    private String brand;
+
+    public String getBrand() { return brand; }
+
+    public void setBrand(String brand) { this.brand = brand; }
+}
